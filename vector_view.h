4e08e79709d484b45a7200e3c057210a264d9453
Beosar <73389112+Beosar@users.noreply.github.com>
2024-04-08 08:42:06 +0200

    Added insert, optimized resize, bugfixes

--- vector_view.h
+++ vector_view.h
@@ -1,4 +1,12 @@
+#pragma once
+
 #include <vector>
+
+#if (defined(_MSVC_LANG) && _MSVC_LANG >= 202002L) || __cplusplus >= 202002L
+#define _CONSTEXPR constexpr
+#else
+#define _CONSTEXPR
+#endif
 
 template<typename T, typename Alloc = std::allocator<T>>
 class vector_view {
@@ -17,18 +25,45 @@ public:
 	using reverse_iterator = std::reverse_iterator<iterator>;
 	using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
-	vector_view(std::vector<T, Alloc>& rVector) :Vector(rVector), itBegin(rVector.begin()), itEnd(rVector.end()) {}
-	vector_view(std::vector<T, Alloc>& rVector, const iterator& itBegin, const iterator& itEnd) :Vector(rVector), itBegin(itBegin), itEnd(itEnd) {}
+	_CONSTEXPR vector_view(std::vector<T, Alloc>& rVector) :Vector(rVector), itBegin(rVector.begin()), itEnd(rVector.end()) {}
+	_CONSTEXPR vector_view(std::vector<T, Alloc>& rVector, const iterator& itBegin, const iterator& itEnd) :Vector(rVector), itBegin(itBegin), itEnd(itEnd) {}
 
 	template<typename... TArgs>
-	void emplace_back(TArgs&&... Args) {
+	_CONSTEXPR void emplace_back(TArgs&&... Args) {
 		const auto lSize = size();
 		itEnd = Vector.emplace(itEnd, std::forward<TArgs>(Args)...) + 1;
 		itBegin = itEnd - (lSize + 1);
 		
 	}
 
-	void reserve(size_t NewSize) {
+	_CONSTEXPR iterator insert(const_iterator it, const T& value) {
+		const auto Size = size();
+		const auto Offset = it - itBegin;
+		it = Vector.insert(it, value);
+		itBegin = it - Offset;
+		itEnd = itBegin + (Size + 1);
+		return it;
+	}
+	
+	_CONSTEXPR iterator insert(const_iterator it, T&& value) {
+		const auto Size = size();
+		const auto Offset = it - itBegin;
+		it = Vector.insert(it, std::move(value));
+		itBegin = it - Offset;
+		itEnd = itBegin + (Size + 1);
+		return it;
+	}
+	
+	_CONSTEXPR iterator insert(const_iterator it, size_type count, const T& value) {
+		const auto Size = size();
+		const auto Offset = it - itBegin;
+		it = Vector.insert(it, count, value);
+		itBegin = it - Offset;
+		itEnd = itBegin + (Size + count);
+		return it;
+	}
+
+	_CONSTEXPR void reserve(size_t NewSize) {
 		if (NewSize > size()) {
 			const auto Offset = itBegin - Vector.begin();
 			const auto OldSize = size();
@@ -38,85 +73,85 @@ public:
 		}
 	}
 
-	void resize(size_t NewSize) {
-		reserve(NewSize);
-		while (NewSize > size()) {
-			emplace_back();
+	_CONSTEXPR void resize(size_t NewSize) {
+		if (NewSize > size()) {
+			reserve(NewSize);
+			insert(itEnd, NewSize - size(), T());
 		}
-		while (NewSize < size()) {
-			pop_back();
+		if (NewSize < size()) {
+			erase(itEnd - (size() - NewSize), itEnd);
 		}
 	}
 
-	size_t size() const noexcept {
+	_CONSTEXPR size_t size() const noexcept {
 		return itEnd - itBegin;
 	}
-	bool empty() const noexcept {
+	_CONSTEXPR bool empty() const noexcept {
 		return size() == 0;
 	}
 
-	T& operator[](size_t Index) noexcept {
+	_CONSTEXPR T& operator[](size_t Index) noexcept {
 		
 		return *(itBegin + Index);
 	}
 
-	const T& operator[](size_t Index) const noexcept {
+	_CONSTEXPR const T& operator[](size_t Index) const noexcept {
 		return *(itBegin + Index);
 	}
 
-	T& back() noexcept {
+	_CONSTEXPR T& back() noexcept {
 		return operator[](size() - 1);
 	}
 
-	const T& back() const noexcept {
+	_CONSTEXPR const T& back() const noexcept {
 		return operator[](size() - 1);
 	}
 
 	
-	iterator begin() noexcept {
+	_CONSTEXPR iterator begin() noexcept {
 		return itBegin;
 	}
-	iterator end() noexcept {
+	_CONSTEXPR iterator end() noexcept {
 		return itEnd;
 	}
 
-	const_iterator begin() const noexcept {
+	_CONSTEXPR const_iterator begin() const noexcept {
 		return itBegin;
 	}
-	const_iterator end() const noexcept {
+	_CONSTEXPR const_iterator end() const noexcept {
 		return itEnd;
 	}
 
-	const_iterator cbegin() const noexcept {
+	_CONSTEXPR const_iterator cbegin() const noexcept {
 		return itBegin;
 	}
-	const_iterator cend() const noexcept {
+	_CONSTEXPR const_iterator cend() const noexcept {
 		return itEnd;
 	}
 
-	reverse_iterator rbegin() noexcept {
-		return itBegin;
-	}
-	reverse_iterator rend() noexcept {
+	_CONSTEXPR reverse_iterator rbegin() noexcept {
 		return itEnd;
+	}
+	_CONSTEXPR reverse_iterator rend() noexcept {
+		return itBegin;
 	}
 
-	const_reverse_iterator rbegin() const noexcept {
-		return itBegin;
-	}
-	const_reverse_iterator rend() const noexcept {
+	_CONSTEXPR const_reverse_iterator rbegin() const noexcept {
 		return itEnd;
+	}
+	_CONSTEXPR const_reverse_iterator rend() const noexcept {
+		return itBegin;
 	}
 
-	const_reverse_iterator crbegin() const noexcept {
-		return itBegin;
-	}
-	const_reverse_iterator crend() const noexcept {
+	_CONSTEXPR const_reverse_iterator crbegin() const noexcept {
 		return itEnd;
+	}
+	_CONSTEXPR const_reverse_iterator crend() const noexcept {
+		return itBegin;
 	}
 
 	template<typename... TArgs>
-	auto emplace(iterator it, TArgs&&... Args) {
+	_CONSTEXPR auto emplace(iterator it, TArgs&&... Args) {
 		const auto Size = size();
 		const auto Offset = it - itBegin;
 		it = Vector.emplace(it, std::forward<TArgs>(Args)...);
@@ -125,7 +160,7 @@ public:
 		return it;
 	}
 
-	auto erase(iterator it) noexcept {
+	_CONSTEXPR auto erase(iterator it) noexcept {
 		const auto Size = size();
 		const auto Offset = it - itBegin;
 		it = Vector.erase(it);
@@ -135,7 +170,7 @@ public:
 	}
 
 	
-	auto erase(iterator itFirst, const iterator& itLast) noexcept {
+	_CONSTEXPR auto erase(iterator itFirst, const iterator& itLast) noexcept {
 		const auto Size = size();
 		const auto Offset = itFirst - itBegin;
 		const size_t NumErased = std::distance(itFirst, itLast);
@@ -145,7 +180,7 @@ public:
 		return itFirst;
 	}
 
-	void pop_back() noexcept {
+	_CONSTEXPR void pop_back() noexcept {
 		erase(itEnd);
 	}
 	
@@ -174,49 +209,49 @@ public:
 	const_vector_view(const vector_view<T, Alloc>& rVectorView) : itBegin(rVectorView.begin()), itEnd(rVectorView.end()) {}
 
 	
-	size_t size() const noexcept {
+	_CONSTEXPR size_t size() const noexcept {
 		return itEnd - itBegin;
 	}
-	bool empty() const noexcept {
+	_CONSTEXPR bool empty() const noexcept {
 		return size() == 0;
 	}
 
 	
-	const T& operator[](size_t Index) const noexcept {
+	_CONSTEXPR const T& operator[](size_t Index) const noexcept {
 		return *(itBegin + Index);
 	}
 
-	const T& back() const noexcept {
+	_CONSTEXPR const T& back() const noexcept {
 		return operator[](size() - 1);
 	}
 
 
-	const_iterator begin() const noexcept {
+	_CONSTEXPR const_iterator begin() const noexcept {
 		return itBegin;
 	}
-	const_iterator end() const noexcept {
+	_CONSTEXPR const_iterator end() const noexcept {
 		return itEnd;
 	}
 
-	const_iterator cbegin() const noexcept {
+	_CONSTEXPR const_iterator cbegin() const noexcept {
 		return itBegin;
 	}
-	const_iterator cend() const noexcept {
+	_CONSTEXPR const_iterator cend() const noexcept {
 		return itEnd;
 	}
 
-	const_reverse_iterator rbegin() const noexcept {
-		return itBegin;
-	}
-	const_reverse_iterator rend() const noexcept {
+	_CONSTEXPR const_reverse_iterator rbegin() const noexcept {
 		return itEnd;
+	}
+	_CONSTEXPR const_reverse_iterator rend() const noexcept {
+		return itBegin;
 	}
 
-	const_reverse_iterator crbegin() const noexcept {
-		return itBegin;
-	}
-	const_reverse_iterator crend() const noexcept {
+	_CONSTEXPR const_reverse_iterator crbegin() const noexcept {
 		return itEnd;
+	}
+	_CONSTEXPR const_reverse_iterator crend() const noexcept {
+		return itBegin;
 	}
 
 
